1c40a06013e19ac65ed46b67af5d4c57a8dc5526
Dmitry <dimonbyt@gmail.com>
2023-10-19 22:55:41 +0300

    Add project files.

---
+++ HttpRequestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static HttpClient httpClient = new HttpClient();
+
+    static async Task Main()
+    {
+        using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://www.google.com");
+
+        using HttpResponseMessage response = await httpClient.SendAsync(request);
+
+        await Console.Out.WriteLineAsync($"Status: {response.StatusCode}");
+
+        await httpClient.SendAsync(request);
+    }
+}
